18b4c51638396eaed9a29ed7c5f3b4013fcebc28
alfred-ye <yejinzhou0505@qq.com>
2020-04-21 19:26:13 +0800

    把评论页面放在食地中

--- pages/food/place.vue
+++ pages/food/place.vue
@@ -88,19 +88,50 @@
 		<!-- 底部评论栏部分开始 -->
 		<view class="cu-bar foot bg-white">
 			<view class="search-form round">
-				<input class="margin-left" @focus="InputFocus" @blur="InputBlur" :adjust-position="false" type="text" placeholder="说点什么吧..." ></input>
+				<input class="margin-left" @focus="InputFocus" :adjust-position="false" type="text" placeholder="说点什么吧..." ></input>
 			</view>
 			<view class="action">
 				<button class="cu-btn bg-red shadow-blur round">评论</button>
 			</view>
 		</view>
 		<!-- 底部评论栏部分结束 -->
+		
+		<!-- 这里是评论的弹窗开始部分 -->
+		<view class="cu-modal bottom-modal" :class="showCommentWindow?'show':''">
+			<view class="cu-dialog eb-comment">
+				<!-- 头部开始 -->
+				<view class="header">
+					<view class="back-btn" @tap="hideCommentWindow()">返回</view>
+					<view class="post-btn" @tap="postComment()">发表评论</view>
+				</view>
+				<!-- 头部结束 -->
+				<!-- 评论填写部分开始 -->
+				<textarea class="content" placeholder="说点什么吧..."></textarea>
+				<!-- 评论填写部分结束 -->
+				<!-- 上传图片部分开始，最多选择三张 -->
+				<view class="img">
+					<!-- 三个预览图 -->
+					<view @tap="chooseImg(1)" class="thumb-img">
+						<image :src="imgs.img1"></image>
+					</view>
+					<view @tap="chooseImg(2)" class="thumb-img">
+						<image :src="imgs.img2"></image>
+					</view>
+					<view @tap="chooseImg(3)" class="thumb-img">
+						<image :src="imgs.img3"></image>
+					</view>
+				</view>
+				<!-- 上传图片部分结束 -->
+			</view>
+		</view>
+		<!-- 这里是评论的弹窗结束部分 -->
 	</view>
 </template>
 
 <script>
 	var _self;
-	import uniRate from '@/components/uni-rate/uni-rate.vue'
+	import uniRate from '@/components/uni-rate/uni-rate.vue';
+	
 	export default {
 		data() {
 			return {
@@ -108,17 +139,25 @@
 					score: 4.8
 				},
 				specialFood:[],
-				commentList: []
-				
+				commentList: [],
+				showCommentWindow: false,
+				imgs: {
+					img1: '',
+					img2: '',
+					img3: '',
+				},
+				filePath: [],
 			}
 		},
 		methods: {
+			// 点击收藏按钮触发的收藏事件
 			collect: function() {
 				_self = this;
 				//_self.place.name = "changed name",
 				_self.place.isLike = !_self.place.isLike,
 				console.log('isLike: '+ this.place.isLike)
 			},
+			// 初始化页面数据函数，用来从接口获取数据并初始化页面元素
 			async initPage(){
 				const comment = await this.$myRequest({
 					url: '/v1/api/comment/getShopCommentByShopId?shopid=',
@@ -138,6 +177,64 @@
 				console.log('info: ', info.data.data);
 				// 给页面的数据赋值
 				this.place =info.data.data;
+			},
+			// 上传图片并根据参数改变对应的URL
+			chooseImg(index) {
+				let imgs = uni.chooseImage({
+				    count: 1,
+				    success: (res) => {
+						switch (index){
+							case 1:
+								this.imgs.img1 = res.tempFilePaths[0];
+								break;
+							case 2:
+								this.imgs.img2 = res.tempFilePaths[0];
+								break;
+							case 3:
+								this.imgs.img3 = res.tempFilePaths[0];
+								break;
+						}
+						this.filePath.push(res.tempFilePaths[0]);
+						console.log('file paths: ', this.filePath);
+					},
+				    });
+			},
+			// 底部评论栏获得焦点时弹出评论窗口
+			InputFocus() {
+				console.log('input focus');
+				this.showCommentWindow = true;
+			},
+			// 按下发表评论按钮时发表评论
+			postComment() {
+				// POST给接口
+				this.$myRequest({
+					url: '/v1/api/comment/insertShopCommentWithUserid',
+					data: {
+						content: 'test',
+						pictures: this.filePath,
+						userid: 123,
+						shopid: 213,
+					},
+					method: 'POST',
+				}),
+				// 控制台显示成功
+				console.log('发表评论成功');
+				// 隐藏弹窗
+				this.hideCommentWindow();
+				// 弹框提示成功
+				uni.showToast({
+					title: '评论发表成功！',
+				});
+				// 清空评论的内容
+				this.clearComment();
+			},
+			// 隐藏评论窗口
+			hideCommentWindow() {
+				this.showCommentWindow = false;
+			},
+			// 清空评论的内容
+			clearComment() {
+				// 有需要再写吧。
 			}
 		},
 		onLoad() {
