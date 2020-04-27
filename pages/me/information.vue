7a45bc1b210e3bb853236a6a2ce8d139f002f5a5
TL <535713459@qq.com>
2020-04-27 18:23:25 +0800

    添加个人信息页面

---
+++ pages/me/information.vue
@@ -0,0 +1,148 @@
+<template>
+	<view>
+		<cu-custom style="background-color: #fff;" :isBack="true">
+			<block slot="backText">返回</block>
+			<block slot="content">
+				<view style="font-size: 20px;">个人资料</view>
+			</block>
+		</cu-custom>
+		
+		<view class="content-column" style="background-color: #fff;">
+			<view>
+				<view>
+					<view v-for="(item,i) in files" :key="i">
+						<view class="weui-uploaderfile" @tap="chooseImage" :id="item">
+							<image class="weui-uploaderimg" :src="item" mode="aspectFill" style="height: 130upx;width: 130upx;border-radius: 50%;" />
+						</view>
+					</view>
+					
+					<view v-if="files.length < 1 && files.length>=0" class="weui-uploader__input-box">  
+						<view class="weui-uploader__input" @tap="chooseImage">  
+							<image :src="personInfo.headportrait" class="img" mode="aspectFill" style="height: 250upx;width: 250upx;border-radius: 50%;"></image>  
+						</view>  
+					</view> 
+				</view>
+			</view>
+		</view>
+		
+		<view class="size cu-form-group margin-top content-column">
+			<view class="flex">
+				<text class="iconfont iconxinzeng" @click="prompt('uni-prompt')">
+					<view style="font-size: 36upx;font-weight: bold;">
+						{{personInfo.name}}
+					</view>
+				</text>
+				<prompt ref="prompt" @onConfirm="onConfirm" @onCancel="onCancel" :text="promptText"></prompt>
+			</view>	
+		</view>
+		
+		<view class="size cu-form-group margin-top content-column">
+			<view class="flex">
+				<text>性别</text>
+			</view>	
+		</view>
+		
+		<view class="size cu-form-group margin-top content-column">
+			<view class="flex">
+				<text>年龄</text>
+			</view>	
+		</view>
+	</view>	
+</template>
+
+<script>
+	import prompt from '@/components/prompt.vue';
+	export default {
+		data() {
+			return {	
+				personInfo: [],
+				files: [],	
+			}
+		},
+		onLoad() {
+			this.initPage()
+		},
+		components: {
+			prompt,
+		},
+		methods: {
+			async initPage(){
+				const res = await this.$myRequest({
+					url: '/v1/api/mypage/getPersonInfo',
+					data: {
+						account: 'oL9hKFg4Ou',
+					}
+				})
+				// 给页面的数据赋值
+				this.personInfo =res.data.data;
+			},
+			prompt:function(){
+				this.$refs.prompt.show();
+			},
+			onConfirm:function(e){
+				console.log(e);
+				let _cost = e;
+				if (_cost == '') {
+				 console.log('你还未输入');
+				 return;
+				}
+				else{
+				  this.$refs.prompt.hide();
+				  uni.showModal({ 
+				  	title: '提示',
+				  	content: '你输入的是:'+_cost,
+				  	showCancel: false,
+				  	confirmText: "确定"
+				  })
+				}
+			},
+			onCancel:function(){
+				this.$refs.prompt.hide();
+				this.$refs.prompt.cost = '';
+			},
+			chooseImage(e){
+				let that = this;
+				uni.chooseImage({
+					count:1,
+					sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+					sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+					success: function (res) {
+						if(res.tempFiles[0].size > 2097152 ){
+							PUBLIC.SHOWTOAST('上传图片不能超过2M~')
+							return false
+						}
+						// 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+						that.files = that.files.concat(JSON.parse(JSON.stringify(res.tempFilePaths)))
+					}
+				})
+			},
+		}
+	}
+</script>
+
+<style>
+	.content-row {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+	.content-column {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.head{
+		height: 200upx;
+		width: 200upx;
+	}
+	.size{
+		font-size: 32upx;
+	}
+	.icon{
+		height: 50upx;
+		width: 50upx;
+		border-radius: 50%;
+	}
+</style>
