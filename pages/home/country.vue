cdd1d4c94526cad99f54d00437f245a43a1a344c
alfred-ye <yejinzhou0505@qq.com>
2020-04-29 17:20:27 +0800

    home系列页面更新

--- pages/home/country.vue
+++ pages/home/country.vue
@@ -3,7 +3,7 @@
 		<view class="uni-flex" style="height: 100upx;color: #fff; padding-top: 30upx;">
 			<view class="center" style="font-size: 42upx;">精选特色菜品</view>
 			<view class="flex justify-end" style="font-size: 25upx;width: 720upx;">
-				<navigator url="../home/city">前往我的城市></navigator>
+				<navigator url="/pages/home/city">前往我的城市></navigator>
 			</view>	
 		</view>
 		
@@ -23,12 +23,11 @@
 		<view style="display: flex;">
 		    <scroll-view scroll-y="true" style="height: 1330upx;flex:1;width:680upx;">
 		        <view v-for="(item,index) in food" class="content" :key="index">
-					<view style=" height:auto; display:flex;flex-direction: column;border-radius: 20upx;"
-		        	class="bg-white content">
+					<view class="bg-white content country-card">
 						<!--点击图片跳转-->
 		        		<image class="picture" @tap="jumpfood(index)" :src="item.picture"></image>
 		        		<view style="display:flex ;flex-direction: column; width: 680upx;">
-		        			<view class="content" style="font-size: 40upx;font-weight: bold">{{item.name}}</view>
+		        			<view class="content title">{{item.name}}</view>
 		        			<view class="text">
 								{{item.introduction}}
 							</view>
@@ -38,6 +37,8 @@
 		        </view>	
 		    </scroll-view>
 		</view>
+		<!-- 底部留白 -->
+		<div class="eb-blank"></div>
 	</view>
 </template>
 
@@ -85,6 +86,15 @@
 </script>
 
 <style>
+	.country-card {
+		display:flex;
+		flex-direction: column;
+		padding: 10upx;
+		height:auto; 
+		border-radius: 4upx;
+		box-shadow: 5upx 5upx 15upx rgba(0, 0, 0, .1);
+		box-sizing: border-box;
+	}
 	.center{
 		display: flex;
 		justify-content: center;
@@ -95,18 +105,24 @@
 		align-items: center;
 		justify-content: center;
 	}
+	.title {
+		margin: 10upx 0;
+		font-size: 35upx;
+		font-weight: bold;
+	}
 	.picture {
 		height: 420upx;
-		width: 680upx;
-		border-radius: 20upx;
+		width: 100%;
 	}
 	.text{
-		padding: 10upx;
+		padding: 0 17upx;
+		width: 100%;
 		colof: #999;
-		max-height: 200upx;
+		line-height: 1.5em;
+		height: 130upx;
 		overflow: scroll;
 	}
 	.page{
-		background: linear-gradient(#F76260,#fff);
+		background: linear-gradient(#e54d42,#fff);
 	}
 </style>
