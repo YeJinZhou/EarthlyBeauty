7ade2c8fba3d8606df9ec9ca0e983010f4f4661a
535713459 <535713459@qq.com>
2020-05-25 16:25:22 +0800

    联调修改真数据传参显示问题

--- pages/food/food.vue
+++ pages/food/food.vue
@@ -4,22 +4,22 @@
 		<cu-custom style="background-color: #fff;" :isBack="true">
 			<block slot="backText">返回</block>
 			<block slot="content">
-				<view style="font-size: 42upx;font-weight: bold;">{{title}}美食</view>
+				<view style="font-size: 42upx;font-weight: bold;">{{cityInfo.name}}美食</view>
 			</block>
 		</cu-custom>
 		<!-- 图片部分 -->
 		<view class="content-row food-pic-part">
 			<!-- 美食主图 -->
 			<view class="food-main-pic">
-				<image :src="foodInfo[0].picture"></image>
+				<image :src="foodInfo[mainFood].pictures[0]"></image>
 			</view>
 			<!-- 滚动图 -->
 			<view class="uni-flex" style="height: 220upx;width: 400upx;">
-				<view class="flex justify-end text-sm">查看{{title}}其他美食 ></view>
+				<view class="flex justify-end text-sm">查看{{cityInfo.name}}其他美食 ></view>
 				<scroll-view scroll-x class="response cu-steps steps-bottom food-pic-scroll">
 					<block v-for="(item,index) in foodInfo" :key="index">
 						<view class="item" @tap="changfood(index)">
-							<view class="cu-avatar round lg" :style="[{ backgroundImage:'url(' + item.picture + ')' }]"></view>
+							<view class="cu-avatar round lg" :style="[{ backgroundImage:'url(' + item.pictures[0] + ')' }]"></view>
 							<text class="text-sm margin-top-xs" style="height: 30upx; display: flex; justify-content: center;">{{item.name}}</text>
 						</view>
 					</block>
@@ -27,27 +27,25 @@
 			</view>
 		</view>
 		<!-- 食物介绍 -->
-		<view class="food-title">{{foodInfo[0].name}}</view>	
+		<view class="food-title">{{foodInfo[mainFood].name}}</view>	
 		<view class="food-desc">			
 			<template v-if="showText">			
-				<view v-if="foodInfo[0].introduction" style="font-size: 32upx;color:#000;">{{foodInfo[0].introduction}}
-					<text v-if="foodInfo[0].introduction !== null && foodInfo[0].introduction.length > 64" @click="toggleDescription" style="color: #f00;">收起</text>
+				<view v-if="foodInfo[mainFood].introduction" style="font-size: 32upx;color:#000;">{{foodInfo[mainFood].introduction}}
+					<text v-if="foodInfo[mainFood].introduction !== null && foodInfo[mainFood].introduction.length > 64" @click="toggleDescription" style="color: #f00;">收起</text>
 				</view>
 			</template>
 			<template v-else>
-				<view v-if="foodInfo[0].introduction" style="font-size: 28upx;color:#000;">{{foodInfo[0].introduction.substr(0, 60)}}
-					<text v-if="foodInfo[0].introduction !== null && foodInfo[0].introduction.length > 64" @click="toggleDescription" style="color: #f00;">
+				<view v-if="foodInfo[mainFood].introduction" style="font-size: 32upx;color:#000;">{{foodInfo[mainFood].introduction.substr(0, 60)}}
+					<text v-if="foodInfo[mainFood].introduction !== null && foodInfo[mainFood].introduction.length > 64" @click="toggleDescription" style="color: #f00;">
 						<text style="color: #000;">...</text>更多						
 					</text>
 				</view>
 			</template>
 		</view>
-		<!-- {{food.foodid}} 这行是啥，先注释了 -->
 		<!-- 店铺列表 -->
 		<scroll-view scroll-y="true" style="height: 950upx;" >
 			<view v-for="(item,index) in placeInfo" :key="index">
-				<navigator url="../food/place" hover-class="navigator-hover">
-					<view class="food-shop">
+					<view class="food-shop" @tap="jumpshop(index)">
 						<image class="food-shop-img" :src="item.pictures[0]" ></image>
 						<view class="food-shop-desc">
 							<view class="text-lg text-bold">
@@ -64,63 +62,122 @@
 							</view>
 							<view>{{item.introduction}}</view>
 						</view>							
-					</view>
-				</navigator>					
+					</view>				
 			</view>
 		</scroll-view>
 	</view>
 </template>
 
 <script>
-	
+	// var mainFood;
+	var food = 0;
+	var f;
+	var city = 0;
+	//console.log(food.foodid);
 	import uniRate from '@/components/uni-rate/uni-rate.vue'
 	export default {
 		data() {
+			console.log(food)
 			return {
-				test:[],
+				// console.log(mainFood),
+				mainFood: food,
 				placeInfo: [],
 				foodInfo: [],
-				food:[],
+				cityInfo: [],
 				showText: false,
-				title:'西安',
 			}
 		},
 		onLoad(e) {
-			this.initPage(),
-			this.init(),
-			this.food=e
+			f = e,
+			console.log(f.foodid),
+			console.log(f.cityid),
+			food = f.foodid,
+			++food,
+			--food,
+			city = f.cityid,
+			++city,
+			--city,
+			console.log(typeof food)
+			console.log(food)
+			console.log(typeof city)
+			console.log(city)
+			this.mainFood = food,
+			this.mainFood--,
+			console.log(typeof this.mainFood)
+			this.mainFood = this.mainFood+7-city*7,
+			console.log(this.mainFood),
+			this.initPage()
+			
+			//this.$forceUpdate() 
 		},
+		// mounted() {
+		// 	this.initPage()
+		// },
 		methods: {
 			async initPage(){
+				var that = this;
+				console.log(city)
 				const res = await this.$myRequest({
 					url: '/v1/api/homepage/getCityFood',
 					data: {
-						cityid: '6463',
+						cityid: city,
 					}
 				})
-				// 给页面的数据赋值
 				this.foodInfo =res.data.data;
-			},
-			async init(){
-				const res = await this.$myRequest({
+				console.log(1)
+				console.log(this.foodInfo[0].name)
+				
+				console.log(food)
+				const res1 = await this.$myRequest({
 					url: '/v1/api/homepage/getShopByFood',
 					data: {
-						foodid: '80121',
+						foodid: food,
 					}
 				})
-				// 给页面的数据赋值
-				this.placeInfo =res.data.data;
-			},
-			changfood(e) {
+				this.placeInfo =res1.data.data;
+				console.log(2)
+				//console.log(this.placeInfo[1].name)
+				console.log(city)
 				
-				var that=this;
-				this.test=that.foodInfo[e];
-				that.foodInfo[e]=that.foodInfo[0];
-				that.foodInfo[0]=this.test;
+				const res2 = await this.$myRequest({
+					url: '/v1/api/homepage/getCityIntroductionById',
+					data: {
+						cityid: city,
+					}
+				})
+				console.log(3)
+				this.cityInfo =res2.data.data;
+				console.log(this.cityInfo.name)
+				
+				// console.log(food)
+				// ++that.mainFood;
+				// --that.mainFood;
+				// that.mainFood = food;
+				// console.log(that.mainfood)
+			},
+			jumpshop(e) {
+				var that = this;
+				var shopid = that.placeInfo[e].id;
+				console.log(shopid);
 				uni.navigateTo({
-				    url: '../food/food',
+				    url: '../food/place?shopid='+shopid,
 				});
 			},
+			
+			changfood(index) {
+				food = index;
+				//this.mainFood = food;
+				//city = index;
+				var that = this;
+				var foodid = that.foodInfo[index].id;
+				var cityid = that.foodInfo[index].cityid;
+				console.log(foodid);
+				console.log(cityid);
+				uni.redirectTo({
+					 url: '../food/food?foodid='+foodid+'&cityid='+cityid,
+				});
+			},
+			
 			toggleDescription (num) {
 				this.showText = !this.showText
 			}
