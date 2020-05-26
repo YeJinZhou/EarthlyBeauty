b68c95babfc3342ee6135f5867dff57fe4e95968
1712190420 <43907127+1712190420@users.noreply.github.com>
2020-05-26 17:51:57 +0800

    增加搜索功能

--- pages/home/country.vue
+++ pages/home/country.vue
@@ -13,94 +13,113 @@
 				<text class="cuIcon-search"></text>
 				<!-- <input @focus="InputFocus" @blur="InputBlur" :adjust-position="false" type="text" 
 				placeholder="请输入搜索内容" confirm-type="search"></input> -->
-				<input type="text" focus v-model="changeVaule" @confirm="searchFun" placeholder="qin"/>
+				<input type="text" focus v-model="search" placeholder="请输入想要搜索的美食"/>
 			</view>
 			<view class="action">
-				<button class="cu-btn bg-red shadow-blur round">搜索</button>
+				<button class="cu-btn bg-red round" @tap="confirm()">搜索</button>
 			</view>
 		</view>
 		<!-- <input type="text" focus v-model="changeVaule" @confirm="searchFun" placeholder="qin"/> -->
-		
-		
-		<!--食物介绍-->
-		<view style="display: flex;">
-		    <scroll-view scroll-y="true" style="height: 1330upx;flex:1;width:680upx;">
-		        <view v-for="(item,index) in food" class="content" :key="index">
-					<view class="bg-white content country-card">
-						<!--点击图片跳转-->
-		        		<image class="picture" @tap="jumpfood(index)" :src="item.pictures[0]"></image>
-		        		<view style="display:flex ;flex-direction: column; width: 680upx;">
-		        			<view class="content title">{{item.name}}</view>
-		        			<view class="text">
-								{{item.introduction}}
+
+		<view v-if="check==true">
+			<!--食物介绍-->
+			<view style="display: flex;">
+				<scroll-view scroll-y="true" style="height: 1330upx;flex:1;width:680upx;">
+					<view v-for="(item,index) in food" class="content" :key="index">
+						<view class="bg-white content country-card">
+							<!--点击图片跳转-->
+							<image class="picture" @tap="jumpfood(index)" :src="item.pictures[0]"></image>
+							<view style="display:flex ;flex-direction: column; width: 680upx;">
+								<view class="content title">{{item.name}}</view>
+								<view class="text">
+									{{item.introduction}}
+								</view>
 							</view>
-		        		</view>
-		        	</view>
-		        	<view style="height: 40upx;"></view>
-		        </view>	
-		    </scroll-view>
+						</view>
+						<view style="height: 40upx;"></view>
+					</view>	
+				</scroll-view>
+			</view>
+			<!-- 底部留白 -->
+			<div class="eb-blank"></div>
 		</view>
-		<!-- 底部留白 -->
-		<div class="eb-blank"></div>
+		<view v-if="check==false">
+			<view v-if="c.code == 0">
+				<!--食物介绍-->
+				<view style="display: flex;">
+					<scroll-view scroll-y="true" style="height: 1330upx;flex:1;width:680upx;">
+						<view v-for="(item,index) in p" class="content" :key="index">
+							<view class="bg-white content country-card">
+								<!--点击图片跳转-->
+								<image class="picture" @tap="jumpfood(index)" :src="item.pictures[0]"></image>
+								<view style="display:flex ;flex-direction: column; width: 680upx;">
+									<view class="content title">{{item.name}}</view>
+									<view class="text">
+										{{item.introduction}}
+									</view>
+								</view>
+							</view>
+							<view style="height: 40upx;"></view>
+						</view>	
+					</scroll-view>
+				</view>
+				<!-- 底部留白 -->
+				<div class="eb-blank"></div>
+			</view>
+			<view v-if="c.code == -1">
+				查找不到
+			</view>
+		</view>
+		
 	</view>
 </template>
 
 <script>
+	var p;
+	var c;
+	var search;
 	export default {
 		data() {
 			return {	
 				showText: false,
 				food: [],
-				changeVaule: '',    // 输入框输入的值
-				searchAll: [],
+				search:'',
+				check: true,
+				code:'',
+				p: [],
+				c: [],
 			}
 		},
 		onLoad() {
-			const than = this        // 注意this的指向
-				uni.getStorage({
-					key: 'searchAll_key',
-					success(res) {
-						than.searchAll = res.data.data
-					}
-				}
-			)
 		},
 		methods: {
-			searchFun() {
-				if(this.changeVaule != '') {    // 输入框的值不为空时
-			        const than = this;
-					this.searchAll.push(this.changeVaule),   // 将输入框的值添加到搜索记录数组中存储
-					uni.setStorage({
-						key: 'searchAll_key',
-						data: than.searchAll,    
-						success: function () {
-							// console.log(changeVaule)
-						}
-					}),
-					this.changeVaule = ''
-				}
+			confirm(){
+				this.searchFood();
+				this.check = false;
+				//console.log(this.check)
 			},
-
+			async searchFood(){
+				//console.log(this.search)
+				const res = await this.$myRequest({
+					url: '/v1/api/homepage/queryFood',
+					data: {
+						foodname:this.search,
+					}
+				})
+				this.c = res.data;
+				this.p = res.data.data;
+				//console.log(c.code)
+				// console.log(p[0].name)
+				// console.log(res.code)
+			},
 			//初始化页面，从接口获取数据
 			async initPage(){
 				const res = await this.$myRequest({
 					url: '/v1/api/homepage/getNationalSelection',
 				})
 				this.food =res.data.data;
-				
-				// const res1 = await this.$myRequest1({
-				// 	url: '/v1/api/homepage/getNationalSelection',
-				// })
-				// this.food =res.data.data;
 			},
-			//搜索框弹出键盘样式修改
-			InputFocus(e) {
-				this.InputBottom = e.detail.height
-			},
-			//搜索框弹出键盘样式修改
-			InputBlur(e) {
-				this.InputBottom = 0
-			},
+			
 			//食物带foodid参数跳转
 			jumpfood(e) {   
 				var that = this;
@@ -112,6 +131,7 @@
 				    url: '../food/food?foodid='+foodid+'&cityid='+cityid,
 				});
 			},
+			
 			//多余文字隐藏
 			toggleDescription (num) {
 				this.showText = !this.showText
