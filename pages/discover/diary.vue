a08c5261ff35c7b28b783d7389669b7e84681385
陈叶斌 <37497999+cyb123456cyb@users.noreply.github.com>
2020-06-05 20:45:45 +0800

    食记页面评论部分更新

--- pages/discover/diary.vue
+++ pages/discover/diary.vue
@@ -73,13 +73,32 @@
 			
 		</view>
 		
-		<view class="dibu">
-			<text class="middle" @click="toggleMask('show')" size="" plain="true"  >说点什么吧...</text>
-			<uni-fav :checked="checkList[0]" class="shoucang" circle="true" bg-color="#fff" bg-color-checked="#e54d42" @click="onClick(0)"></uni-fav>
-			<ygc-comment ref="ygcComment"
-			    :placeholder="'发布评论'" 
-				@pubComment="pubComment"></ygc-comment>
+		<view class="cu-bar foot bg-white">
+			<view class="search-form round">
+				<input class="margin-left" @focus="InputFocus" :adjust-position="false" type="text" placeholder="说点什么吧..." ></input>
+			</view>
+			<view class="action">
+				<button class="cu-btn bg-red shadow-blur round">收藏</button>
+			</view>
 		</view>
+		<!-- 底部评论栏部分结束 -->
+		
+		<!-- 这里是评论的弹窗开始部分 -->
+		<view class="cu-modal bottom-modal" :class="showCommentWindow?'show':''">
+			<view class="cu-dialog eb-comment">
+				<!-- 头部开始 -->
+				<view class="header">
+					<view class="back-btn" @tap="hideCommentWindow()">返回</view>
+					<view class="post-btn" @tap="postComment()">发表评论</view>
+				</view>
+				<!-- 头部结束 -->
+				<!-- 评论填写部分开始 -->
+				<textarea v-model="newComment" class="comment-content" placeholder="说点什么吧..."></textarea>
+				<!-- 评论填写部分结束 -->
+				
+			</view>
+		</view>
+
 	</view>
 </template>
 
@@ -98,59 +117,26 @@ export default {
 	data() {
 		
 		return {
+			userid: '982157286@qq.com',
+			newComment: '',
 			showPlan:false,
-			
+			showCommentWindow: false,
 			checkList: [false],
 			//是否显示返回按钮
 			// #ifndef MP
 			showBack:true,
 			// #endif
 			//轮播主图数据
-			swiperList: [
-				{ id: 1, img: '/static/23.png' },
-				{ id: 2, img: '/static/2.jpg' },
-				{ id: 3, img: '/static/3.jpg' },
-				{ id: 4, img: '/static/4.jpg' }
-			],
+			
 			//轮播图下标
 			currentSwiper: 0,
 			anchorlist:[],//导航条锚点
 			selectAnchor:0,//选中锚点
-			serviceClass: '',//服务弹窗css类，控制开关动画
-			specClass: '',//规格弹窗css类，控制开关动画
-			shareClass:'',//分享弹窗css类，控制开关动画
-			
-			biaoqian:{
-				tag:'#西安#'
-			},
-			
-			user:{
-				name:'随芝所乐',
-				face:'/static/face.jpg',
-				xinxi:'说到西安，你脑袋中第一浮现出来的是什么呢？我脑袋中浮现出来的就是美食！！！大凡到过西安的人，都会去品尝一下西安的牛羊肉泡馍。由于羊肉泡馍经济实惠，而极富有地方特色，来西安不吃牛羊肉泡，似乎就白来西安一趟.',
-			},
-			
-			// 评论信息
-			goodsData:{
-				id:1,
-				
-				
-				comment:{
-					userface:'/static/face.jpg',
-					username:'大黑哥',
-					content:'看起来很好吃啊，感谢分享！我也好喜欢西安这座城市！西安带给我好多美好的回忆！'
-				}
-			},
-			
 			backendData:[
 				{
 					id:1,
-					userBriefInformation:{},
-					
+					userBriefInformation:{},	
 				}
-					
-				
-		
 			],
 			
 			commentData:[
@@ -172,24 +158,32 @@ export default {
 		console.log(this.idData);
 		
 	},
-	
-	// onLoad:function(options){
-	// 	this.setData({
-	// 	      foodrecordid: foodrecordid    
-	// 	    })
-	// },
-	
-	onReady(){
-		//this.calcAnchor();//计算锚点高度，页面数据是ajax加载时，请把此行放在数据渲染完成事件中执行以保证高度计算正确
-	},
-	onPageScroll(e) {
-		
-	},
-	
+
 	mounted () {
 		
 	},
 	methods: {
+		
+		async postComment() {
+			const res1 =await this.$myRequest({
+				
+				url:'/v1/api/comment/insertFoodRecordCommentWithUserid',
+				data:{
+					content:this.newComment,
+					foodrecordid:this.backendData.id,
+					userid:'472296000@qq.com'
+				},
+				//methods:"POST"
+			}) 
+			console.log('-shop id: ' + this.backendData.id);
+			console.log('-user id: ' + this.userid);
+			console.log('-content: ' + this.newComment);
+			// 隐藏弹窗
+			this.hideCommentWindow();
+			this.initPage(this.backendData.id);
+			// 清空评论的内容
+			//this.clearComment();
+		},
 		
 		async initPage(foodrecordid){
 			const res =await this.$myRequest({
@@ -220,7 +214,7 @@ export default {
 			
 		  
 			console.log(this.idData);
-			
+			console.log(this.backendData.id);
 		},
 		
 		changeStatus(){
@@ -234,17 +228,20 @@ export default {
 			this.currentSwiper = event.detail.current;
 		},
 		
+		InputFocus() {
+			console.log('input focus');
+			this.showCommentWindow = true;
+		},
+		hideCommentWindow() {
+			this.showCommentWindow = false;
+		},
+		
 		onClick(index) {
 			this.checkList[index] = !this.checkList[index]
 			this.$forceUpdate()
 		},
 		
-		//评论，跳转页面
-		toRatings(){
-			uni.navigateTo({
-				url:'ratings/ratings'
-			})
-		},
+		
 		
 		tocreatePost(){
 			uni.navigateTo({
@@ -256,15 +253,8 @@ export default {
 		back() {
 			uni.navigateBack();
 		},
-		toggleMask(type) {
-			this.$refs.ygcComment.toggleMask(type);
-		},
-		pubComment(commentContent1) {
-			this.$refs.ygcComment.toggleMask();
-			console.log(commentContent1);
-			this.commentContent = commentContent1;
-			this.$refs.ygcComment.content = '';
-		}
+		
+		
 		
 		
 	}
@@ -285,38 +275,7 @@ page {
 	border-radius: 50%;
 	background-color: rgba(0, 0, 0, .3);
 }
-@keyframes showPopup {
-		0% {
-			opacity: 0;
-		}
-		100% {
-			opacity: 1;
-		}
-	}
-	@keyframes hidePopup {
-		0% {
-			opacity: 1;
-		}
-		100% {
-			opacity: 0;
-		}
-	}
-	@keyframes showLayer {
-		0% {
-			transform: translateY(0);
-		}
-		100% {
-			transform: translateY(-100%);
-		}
-	}
-	@keyframes hideLayer {
-		0% {
-			transform: translateY(-100%);
-		}
-		100% {
-			transform: translateY(0);
-		}
-	}
+
  
 .button_1{
 	margin: 10upx auto;
@@ -366,130 +325,8 @@ page {
 .icon {
 	font-size: 26upx;
 }
-.status {
-	width: 100%;
-	height: 0;
-	position: fixed;
-	z-index: 10;
-	top: 0;
-	/*  #ifdef  APP-PLUS  */
-	height: var(--status-bar-height); //覆盖样式
-	/*  #endif  */
-	background-color: #f1f1f1;
-	transition: opacity 0.05s linear;
-}
-.header {
-	width: 100%;
 
-	height: 100upx;
-	display: flex;
-	align-items: center;
-	position: fixed;
-	top: 0;
-	z-index: 10;
-	/*  #ifdef  APP-PLUS  */
-	top: var(--status-bar-height);
-	/*  #endif  */
-	.before,
-	.after {
-		width: 92%;
-		padding: 0 4%;
-		height: 100upx;
-		display: flex;
-		align-items: center;
-		position: fixed;
-		top: 0;
-		/*  #ifdef  APP-PLUS  */
-			top: var(--status-bar-height);
-		/*  #endif  */
-		transition: opacity 0.05s linear;
-		.back {
-			width: 125upx;
-			height: 60upx;
-			flex-shrink: 0;
-			.icon {
-				margin-left: -10%;
-				width: 60upx;
-				height: 60upx;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-				font-size: 42upx;
-			}
-		}
-		.middle {
-			width: 100%;
-		}
-		.icon-btn {
-			width: 125upx;
-			height: 60upx;
-			flex-shrink: 0;
-			display: flex;
-			.icon {
-				&:first-child{
-					margin-right: 5upx;
-				}
-				width: 60upx;
-				height: 60upx;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				font-size: 42upx;
-			}
-		}
-	}
-	.before {
-		.back {
-			.icon {
-				color: #fff;
-				background-color: rgba(0, 0, 0, 0.2);
-				border-radius: 100%;
-			}
-		}
-		.icon-btn {
-			.icon {
-				color: #fff;
-				background-color: rgba(0, 0, 0, 0.2);
-				border-radius: 100%;
-				
-			}
-		}
-	}
-	.after {
-		background-color: #f1f1f1;
-		.back {
-			.icon {
-				color: #666;
-			}
-		}
-		.icon-btn {
-			.icon {
-				color: #666;
-			}
-		}
-		.middle {
-			font-size: 32upx;
-			height: 90upx;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			padding: 0 7%;
-			view {
-				width: (100%/3);
-				padding: 0 3%;
-				margin: 0 3%;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				&.on {
-					margin-bottom: -4upx;
-					color: #f47952;
-					border-bottom: solid 4upx #f47952;
-				}
-			}
-		}
-	}
-}
+
 .swiper-box {
 	position: relative;
 	width: 100%;
@@ -590,60 +427,8 @@ page {
 		border-radius: 28upx;
 	}
 }
-.goods-info {
-	.price {
-		font-size: 46upx;
-		font-weight: 600;
-		color: #f47925;
-	}
-	.title {
-		font-size: 30upx;
-	}
-}
-.spec {
-	.row {
-		width: 100%;
-		display: flex;
-		align-items: center;
-		margin: 0 0 30upx 0;
-		.text {
-			font-size: 24upx;
-			color: #a2a2a2;
-			margin-right: 20upx;
-		}
-		.content {
-			font-size: 28upx;
-			display: flex;
-			flex-wrap: wrap;
-			.serviceitem{
-				margin-right: 10upx;
-			}
-			.sp {
-				width: 100%;
-				display: flex;
-				view {
-					background-color: #f6f6f6;
-					padding: 5upx 10upx;
-					color: #999;
-					margin-right: 10upx;
-					font-size: 20upx;
-					border-radius: 5upx;
-					&.on{
-						border: solid 1upx #f47952;
-						padding: 4upx 8upx;
-					}
-				}
-			}
-		}
-		.arrow {
-			position: absolute;
-			right: 4%;
-			.icon {
-				color: #ccc;
-			}
-		}
-	}
-}
+
+
 .comments {
 	margin-bottom: 100upx;
 	.row {
@@ -711,300 +496,8 @@ page {
 		color: #999;
 	}
 }
-.footer {
-	position: fixed;
-	bottom: 0upx;
-	width: 92%;
-	padding: 0 4%;
-	height: 99upx;
-	border-top: solid 1upx #eee;
-	background-color: #fff;
-	z-index: 2;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	.icons {
-		display: flex;
-		height: 80upx;
-		margin-left: -4%;
-		.box {
-			width: 80upx;
-			height: 80upx;
-			display: flex;
-			justify-content: center;
-			flex-wrap: wrap;
-			.icon {
-				font-size: 40upx;
-				color: #828282;
-			}
-			.text {
-				display: flex;
-				justify-content: center;
-				width: 100%;
-				font-size: 22upx;
-				color: #666;
-			}
-		}
-	}
-	.btn {
-		height: 80upx;
-		border-radius: 40upx;
-		overflow: hidden;
-		display: flex;
-		margin-right: -2%;
-		.joinCart,
-		.buy {
-			height: 80upx;
-			padding: 0 40upx;
-			color: #fff;
-			display: flex;
-			align-items: center;
-			font-size: 28upx;
-		}
-		.joinCart {
-			background-color: #f0b46c;
-		}
-		.buy {
-			background-color: #f06c7a;
-		}
-	}
-}
-.popup {
-	position: fixed;
-	top: 0;
-	width: 100%;
-	height: 100%;
-	z-index: 20;
-	display: none;
-	.mask{
-		position: fixed;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		z-index: 21;
-		background-color: rgba(0, 0, 0, 0.6);
-	}
-	.layer {
-		position: fixed;
-		z-index: 22;
-		bottom: -70%;
-		width: 92%;
-		padding: 0 4%;
-		height: 70%;
-		border-radius: 20upx 20upx 0 0;
-		background-color: #fff;
-		display: flex;
-		flex-wrap: wrap;
-		align-content: space-between;
-		.content {
-			width: 100%;
-			padding: 20upx 0;
-		}
-		.btn {
-			width: 100%;
-			height: 100upx;
-			.button {
-				width: 100%;
-				height: 80upx;
-				border-radius: 40upx;
-				color: #fff;
-				display: flex;
-				align-items: center;
-				justify-content: center;
-				background-color: #f47952;
-				font-size: 28upx;
-			}
-		}
-	}
-	
-	&.show {
-		display: block;
-		.mask{
-			animation: showPopup 0.2s linear both;
-		}
-		.layer {
-			animation: showLayer 0.2s linear both;
-		}
-	}
-	&.hide {
-		display: block;
-		.mask{
-			animation: hidePopup 0.2s linear both;
-		}
-		
-		.layer {
-			animation: hideLayer 0.2s linear both;
-		}
-	}
-	&.none {
-		display: none;
-	}
-	&.service {
-		.row {
-			margin: 30upx 0;
-			.title {
-				font-size: 30upx;
-				margin: 10upx 0;
-			}
-			.description {
-				font-size: 28upx;
-				color: #999;
-			}
-		}
-	}
-	&.spec {
-		.title {
-			font-size: 30upx;
-			margin: 30upx 0;
-		}
-		.sp {
-			display: flex;
-			view {
-				font-size: 28upx;
-				padding: 5upx 20upx;
-				border-radius: 8upx;
-				margin: 0 30upx 20upx 0;
-				background-color: #f6f6f6;
-				&.on {
-					padding: 3upx 18upx;
-					border: solid 1upx #f47925;
-				}
-			}
-		}
-		.length{
-			margin-top: 30upx;
-			border-top: solid 1upx #aaa;
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-			padding-top: 20upx;
-			.text{
-				font-size: 30upx;
-			}
-			.number{
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				.input{
-					width: 80upx;
-					height: 60upx;
-					margin: 0 10upx;
-					background-color: #f3f3f3;
-					display: flex;
-					justify-content: center;
-					align-items: center;
-					text-align: center;
-					input{
-						width: 80upx;
-						height: 60upx;
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						text-align: center;
-						font-size: 26upx;
-					}
-				}
-				
-				.sub ,.add{
-					width: 60upx;
-					height: 60upx;
-					background-color: #f3f3f3;
-					border-radius: 5upx;
-					.icon{
-						font-size: 30upx;
-						width: 60upx;
-						height: 60upx;
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						
-					}
-				}
-			}
-		}
-		
-	}
-}
-.share{
-	display: none;
-	&.show {
-		display: block;
-		.mask{
-			animation: showPopup 0.15s linear both;
-		}
-		.layer {
-			animation: showLayer 0.15s linear both;
-		}
-	}
-	&.hide {
-		display: block;
-		.mask{
-			animation: hidePopup 0.15s linear both;
-		}
-		
-		.layer {
-			animation: hideLayer 0.15s linear both;
-		}
-	}
-	&.none {
-		display: none;
-	}
-	.mask{
-		background-color: rgba(0,0,0,.5);
-		position: fixed;
-		width: 100%;
-		height: 100%;
-		top:0;
-		z-index: 11;
-	}
-	.layer{
-		width: 92%;
-		position: fixed;
-		z-index: 12;
-		padding: 0 4%;
-		top: 100%;
-		background-color: rgba(255,255,255,0.9);
-		.list{
-			width: 100%;
-			display: flex;
-			padding:10upx 0 30upx 0;
-			.box{
-				width: 25%;
-				display: flex;
-				justify-content: center;
-				flex-wrap: wrap;
-				image{
-					width: 13.8vw;
-					height: 13.8vw;
-				}
-				.title{
-					margin-top: 10upx;
-					display: flex;
-					justify-content: center;
-					width: 100%;
-					font-size: 26upx;
-				}
-			}
-		}
-		.btn{
-			width: 100%;
-			height: 100upx;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			font-size: 28upx;
-			border-top: solid 1upx #666666;
-		}
-		.h1{
-			width: 100%;
-			height: 80upx;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			font-size: 34upx;
-		}
-	}
-}
+
+
 
 
 </style>
